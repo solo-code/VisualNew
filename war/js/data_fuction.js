acef385d1f5c73a434e32a3d11543824d1242a3b
solo-code <solocode@sina.com>
2015-03-25 17:04:03 +0800

    "所有目录的拷贝"
    “拷贝”

---
+++ war/js/data_fuction.js
@@ -0,0 +1,5 @@
+//定义数据结构：
+		var xLabel = new Array("","x0","x1","x2","x3","x4","x5","x6","x7","x8","x9","x10");
+		var yLabel = new Array("","y0","y1","y2","y3","y4","y5","y6","y7","y8","y9","y10");
+		
+		
